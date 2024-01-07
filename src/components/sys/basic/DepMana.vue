79ac17fe7e134e43053b3432868e678563e7705e
25086 <2508611101@qq.com>
2024-01-07 22:26:37 +0800

    p48未完

--- src/components/sys/basic/DepMana.vue
+++ src/components/sys/basic/DepMana.vue
@@ -33,6 +33,33 @@
                 </span>
              </span>
         </el-tree>
+        <el-dialog
+            title="添加部门"
+            :visible.sync="dialogVisible"
+            width="30%">
+            <div>
+                <table>
+                    <tr>
+                        <td>
+                            <el-tag>上级部门</el-tag>
+                        </td>
+                        <td>{{pname}}</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <el-tag>部门名称</el-tag>
+                        </td>
+                        <td>
+                            <el-input v-model="dep.name" placeholder="请输入部门名称"></el-input>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="doAddDep">确 定</el-button>
+            </span>
+        </el-dialog>
     </div>
 </template>
 
@@ -46,7 +73,13 @@ export default {
             defaultProps:{
                 children:'children',
                 label:'name'
-            }
+            },
+            dialogVisible:false,
+            dep:{
+                name:'',
+                parentId:-1,
+            },
+            pname:''
         }
     },
     watch: {
@@ -58,11 +91,76 @@ export default {
         this.initDeps();
     },
     methods:{
+        initDep(){
+          this.dep={
+              name:'',
+              parentId: -1
+          }
+          this.pname = '';
+        },
+        addDep2Deps(deps, dep){
+            for (let i=0; i < deps.length;i++){
+                let d = deps[i];
+                if(d.id == dep.parentId){
+                    d.children = d.children.concat(dep);
+                    if(d.children.length > 0){
+                        d.isParent = true;
+                    }
+                    return;
+                }else{
+                    this.addDep2Deps(d.children, dep);
+                }
+            }
+        },
+        doAddDep(){
+          this.postRequest('/system/basic/department/', this.dep).then(resp=>{
+              if(resp){
+                  this.addDep2Deps(this.deps, resp.obj);
+                  this.initDeps();
+                  this.dialogVisible = false;
+              }
+          })
+        },
         showAddDep(data){
-            console.log(data);
+            this.dep.parentId = data.id;
+            this.pname = data.name;
+            this.dialogVisible = true;
         },
         deleteDep(data){
-            console.log(data);
+            if(data.isParent){
+                this.$message.error('父部门删除失败!');
+            }else{
+                this.$confirm('此操作将永久删除[' + data.name + ']部门,是否继续?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    this.deleteRequset('/system/basic/department/' + data.id).then(resp => {
+                        if (resp) {
+                            this.removeDepFromDeps(null, this.deps, data.id);
+                        }
+                    })
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '已取消删除'
+                    });
+                });
+            }
+        },
+        removeDepFromDeps(deps, id){
+            for(let i = 0; i < deps.length; i++){
+                let d = deps[i];
+                if(d.id == id){
+                    deps.splice(i, 1);
+                    if(deps.length == 0){
+                        p.isParent = false;
+                    }
+                    return;
+                }else{
+                    this.removeDepFromDeps(d, d.children, id);
+                }
+            }
         },
         initDeps(){
             this.getRequest('/system/basic/department/').then(resp=>{
